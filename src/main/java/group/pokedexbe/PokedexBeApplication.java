a694a8d79eb32dbff9346315b9407ddea4ac984f
roy-hendry <royjairusraj@hotmail.com>
2023-04-29 15:02:08 +0100

    Initial commit

---
+++ src/main/java/group/pokedexbe/PokedexBeApplication.java
@@ -0,0 +1,13 @@
+package group.pokedexbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexBeApplication.class, args);
+	}
+
+}
