ca25160420c83085de2a43165095a7dbc687245a
roy-hendry <royjairusraj@hotmail.com>
2023-05-13 12:59:35 +0100

    Created ResourceNotFound exception

--- src/main/java/group/pokedexbe/PokedexBeApplication.java
+++ src/main/java/group/pokedexbe/PokedexBeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PokedexBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PokedexBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexBeApplication.class, args);
+    }
 
 }
---
+++ src/main/java/group/pokedexbe/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,46 @@
+package group.pokedexbe.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// Defines to Spring what type of error status to return
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    // Defining the name for the resource in this example the model is Pokemon
+    private String resourceName;
+
+    // Defines the field variable we want to show is not found like id (in model class)
+    private String fieldName;
+
+    // Define the value that cannot be found
+    private long fieldValue;
+
+
+    // Constructor for the class
+    public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
+
+        // This will show the exception in string format e.g. 'Pokemon not found with id: -10'
+        super(String.format("%s not found with %s: %s", resourceName, fieldName, fieldValue));
+
+        this.resourceName = resourceName;
+
+        this.fieldName = fieldName;
+
+        this.fieldValue = fieldValue;
+    }
+
+    // Getters because for some reason lombok isn't letting me use the annotation here
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public long getFieldValue() {
+        return fieldValue;
+    }
+
+}
