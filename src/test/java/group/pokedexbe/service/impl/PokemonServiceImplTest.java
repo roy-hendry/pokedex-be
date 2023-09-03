1fea9f35adcc323a6f59942b9e1d8f2043a0b09d
roy-hendry <royjairusraj@hotmail.com>
2023-09-03 15:34:50 +0100

    Made tests for getAllPokemon

--- src/test/java/group/pokedexbe/service/impl/PokemonServiceImplTest.java
+++ src/test/java/group/pokedexbe/service/impl/PokemonServiceImplTest.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -30,7 +33,7 @@ class PokemonServiceImplTest {
     @DisplayName("Testing that whatever the values of the DTO we send in are the same as what we get back")
     @Test
     void createPokemon_happyPathOne() {
-//        PokemonDTO pokemonDTO = PokemonDTO.builder()
+//        PokemonDTO pokemonDTO = Pokem0onDTO.builder()
 //                .id(123L)
 //                .name("Pikabloo")
 //                .isCaught(false)
@@ -65,17 +68,122 @@ class PokemonServiceImplTest {
 //        assertTrue(new ReflectionEquals(pokemonDTO).matches(pokemonDTO));
     }
 
+    @DisplayName("" +
+            "WHEN calling getAllPokemon; " +
+            "GIVEN there are are pokemon in the repository; " +
+            "THEN returns a list containing those pokemon"
+    )
     @Test
-    void getAllPokemon() {
+    void getAllPokemon_happyPathOne() {
+        List<PokemonDTO> expectedPokemonDTOList = Arrays.asList(
+
+                // Arrange
+                PokemonDTO.builder()
+                        .id(1L)
+                        .name("Bulbasaur")
+                        .isCaught(false)
+                        .basicSprite("basicSprite")
+                        .detailedSprite("detailedSprite1")
+                        .abilityName1("abilityName11")
+                        .abilityName2("abilityName12")
+                        .hp(100)
+                        .attack(111)
+                        .defence(222)
+                        .specialAttack(333)
+                        .specialDefence(444)
+                        .speed(5)
+                        .build(),
+                PokemonDTO.builder()
+                        .id(2L)
+                        .name("Squirtle")
+                        .isCaught(false)
+                        .basicSprite("basicSprite")
+                        .detailedSprite("detailedSprite2")
+                        .abilityName1("abilityName21")
+                        .abilityName2("abilityName22")
+                        .hp(100)
+                        .attack(111)
+                        .defence(222)
+                        .specialAttack(333)
+                        .specialDefence(444)
+                        .speed(5)
+                        .build()
+        );
+
+        List<Pokemon> allPokemon = Arrays.asList(
+                Pokemon.builder()
+                        .id(1L)
+                        .name("Bulbasaur")
+                        .isCaught(false)
+                        .basicSprite("basicSprite")
+                        .detailedSprite("detailedSprite1")
+                        .abilityName1("abilityName11")
+                        .abilityName2("abilityName12")
+                        .hp(100)
+                        .attack(111)
+                        .defence(222)
+                        .specialAttack(333)
+                        .specialDefence(444)
+                        .speed(5)
+                        .build(),
+                Pokemon.builder()
+                        .id(2L)
+                        .name("Squirtle")
+                        .isCaught(false)
+                        .basicSprite("basicSprite")
+                        .detailedSprite("detailedSprite2")
+                        .abilityName1("abilityName21")
+                        .abilityName2("abilityName22")
+                        .hp(100)
+                        .attack(111)
+                        .defence(222)
+                        .specialAttack(333)
+                        .specialDefence(444)
+                        .speed(5)
+                        .build()
+        );
+
+        when(pokemonRepositoryMock.findAll()).thenReturn(allPokemon);
+
+        // Act
+        List<PokemonDTO> result = pokemonServiceImpl.getAllPokemon();
+
+        // Assert
+        assertTrue(new ReflectionEquals(expectedPokemonDTOList.get(0)).matches(result.get(0)));
+        assertTrue(new ReflectionEquals(expectedPokemonDTOList.get(1)).matches(result.get(1)));
+
+        verify(pokemonRepositoryMock).findAll();
     }
 
     @DisplayName("" +
-            "WHEN calling getPokemonById" +
-            "GIVEN an id of a pokemon in the repository" +
+            "WHEN calling getAllPokemon; " +
+            "GIVEN there are no pokemon in the repository; " +
+            "THEN returns an empty list"
+    )
+    @Test
+    void getAllPokemon_happyPathTwo() {
+
+        // Arrange
+        List<PokemonDTO> expectedPokemonDTOList = Collections.emptyList();
+        List<Pokemon> allPokemon = Collections.emptyList();
+
+        when(pokemonRepositoryMock.findAll()).thenReturn(allPokemon);
+
+        // Act & Assert
+        assertTrue(expectedPokemonDTOList.equals(pokemonServiceImpl.getAllPokemon()));
+
+        verify(pokemonRepositoryMock).findAll();
+    }
+
+    @DisplayName("" +
+            "WHEN calling getPokemonById; " +
+            "GIVEN an id of a pokemon in the repository; " +
             "THEN returns a DTO generated from that pokemon"
     )
     @Test
     void getPokemonById_happyPathOne() {
+
+        // Arrange
         Pokemon pokemonInRepo = Pokemon.builder()
                 .id(123L)
                 .name("Pikabloo")
@@ -110,20 +218,24 @@ class PokemonServiceImplTest {
 
         when(pokemonRepositoryMock.findById(123L)).thenReturn(Optional.ofNullable(pokemonInRepo));
 
+        // Act & Assert
         assertTrue(new ReflectionEquals(expectedPokemonDTO).matches(pokemonServiceImpl.getPokemonById(123L)));
 
         verify(pokemonRepositoryMock).findById(123L);
     }
 
     @DisplayName("" +
-            "WHEN calling getPokemonById" +
-            "GIVEN an id of a pokemon not in the repository" +
-            "THEN throws a ResourceNotFound exception"
+            "WHEN calling getPokemonById; " +
+            "GIVEN an id of a pokemon not in the repository; " +
+            "THEN throws a ResourceNotFound exception; "
     )
     @Test
     void getPokemonById_sadPathOne() {
+
+        // Arrange
         when(pokemonRepositoryMock.findById(404L)).thenReturn(Optional.empty());
 
+        // Act & Assert
         assertThrows(ResourceNotFoundException.class, () -> {
             pokemonServiceImpl.getPokemonById(404L);
         });
